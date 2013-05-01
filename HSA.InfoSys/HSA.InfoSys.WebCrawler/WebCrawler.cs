54765f8e97248d510eb04997a9f71ca4ff051f7d
Michael Jünger <michael.juenger1@hs-augsburg.de>
2013-05-01 19:22:31 +0000

    Verweise angepasst und damit Warnmeldungen behoben. Im WebCrawler Eingabemethode korrigiert, Server hängt jetzt nicht mehr in der Schleife und wartet auf Eingabe sondern prüft Eingabe erst wenn diese im Inputbuffer vorliegt und arbeitet ansonsten weiter.

--- HSA.InfoSys/HSA.InfoSys.WebCrawler/WebCrawler.cs
+++ HSA.InfoSys/HSA.InfoSys.WebCrawler/WebCrawler.cs
@@ -5,6 +5,7 @@
     using System.Net;
     using System.Threading;
     using HSA.InfoSys.Logging;
+    using HSA.InfoSys.DBManager;
     using log4net;
 
     /// <summary>
@@ -25,23 +26,28 @@
             log.Debug("Starting server...");
             log.Info("Press q for quit.");
 
-            //DBManager dbm = new DBManager();
-
-            
-            
+            DBManager dbm = new DBManager();
 
             while(running)
             {
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-                if (keyInfo.Key == ConsoleKey.Q)
+                if (Console.KeyAvailable)
                 {
-                    running = false;
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    log.InfoFormat("Got user input key {0}", keyInfo.Key);
+
+                    if (keyInfo.Key == ConsoleKey.Q)
+                    {
+                        log.Info("User exited the application.");
+                        running = false;
+                    }
+                    else
+                    {
+                        log.Info("Unkown user input.");
+                    }
                 }
 
-                Thread.Sleep(100); 
+                Thread.Sleep(100);
             }
-            
         }
     }
 }
