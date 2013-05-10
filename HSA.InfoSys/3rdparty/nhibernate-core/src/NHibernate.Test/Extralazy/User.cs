4cd83804216e459e7a959be8703c58f7fe11b8fc
Michael Jünger <michael.juenger1@hs-augsburg.de>
2013-05-10 16:36:32 +0000

    Umstellung auf neue Version von NHibernate wh dotnet 4

---
+++ HSA.InfoSys/3rdparty/nhibernate-core/src/NHibernate.Test/Extralazy/User.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.Extralazy
+{
+	public class User
+	{
+		private string name;
+		private string password;
+		private IDictionary session = new Hashtable();
+		private ISet<Document> documents = new HashSet<Document>();
+		private ISet<Photo> photos = new HashSet<Photo>();
+		protected User() {}
+		public User(string name, string password)
+		{
+			this.name = name;
+			this.password = password;
+		}
+
+		public virtual string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public virtual string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+
+		public virtual IDictionary Session
+		{
+			get { return session; }
+			set { session = value; }
+		}
+
+		public virtual ISet<Document> Documents
+		{
+			get { return documents; }
+			set { documents = value; }
+		}
+
+
+		public virtual ISet<Photo> Photos
+		{
+			get { return photos; }
+			set { photos = value; }
+		}
+	}
+}
