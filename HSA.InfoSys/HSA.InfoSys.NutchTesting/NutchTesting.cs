16e6876afbc6ead677cc465b3506343d02d943bd
André Majeres <andre.majeres@hs-augsburg.de>
2013-05-14 11:42:54 +0000

    Projekt nutchtesting hinzugefügt.

---
+++ HSA.InfoSys/HSA.InfoSys.NutchTesting/NutchTesting.cs
@@ -0,0 +1,65 @@
+﻿namespace HSA.InfoSys.NutchTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using HSA.InfoSys.DBManager.Data;
+    using System.Threading;
+    using log4net;
+    using HSA.InfoSys.Logging;
+    using HSA.InfoSys.DBManager;
+
+    public class NutchTesting
+    {
+        static void Main(string[] args)
+        {
+            ILog log = Logging.GetLogger("NutchTesting");
+
+            bool running = true;
+
+            Console.WriteLine("");
+            Console.WriteLine("Here you can test the nutch funcionality.");
+            Console.WriteLine("To see your options press h or press q for quit.");
+            Console.WriteLine("");
+
+            while (running)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    log.InfoFormat("Key [{0}] was pressed.", keyInfo.Key);
+
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.H:
+                            log.Info("Print help text.");
+                            PrintHelp();
+                            break;
+
+                        case ConsoleKey.Q:
+                            log.Info("Quit application.");
+                            running = false;
+                            break;
+
+                        case ConsoleKey.S:
+                            log.Info("Send request to nutch.");
+                            break;
+                    }
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press h to see this help text.");
+            Console.WriteLine("Press q to quit this application.");
+            Console.WriteLine("Press s to start a new request to nutch server.");
+
+            Console.WriteLine("");
+        }
+    }
+}
