4baa1669ebf800bf1e9dd7a6d9c66100a0755db9
Andreas Fleichaus <andreas.fleichaus@hs-augsburg.de>
2013-06-12 11:29:23 +0000

    Class Result commited to the SolrClient, which has "content","url" and "timestamp".

---
+++ HSA.InfoSys/HSA.InfoSys.SolrClient/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSA.InfoSys.Common.SolrClient
+{
+    class Result
+    {
+        string content = null;
+        string url = null;
+        string tstamp = null;
+    }
+}
