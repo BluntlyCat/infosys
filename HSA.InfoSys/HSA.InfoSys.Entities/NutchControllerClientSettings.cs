0d35c7ccd51a097eb8d220875e9c91d69a9ee866
Michael Jünger <michael.juenger1@hs-augsburg.de>
2013-06-26 20:44:47 +0000

    - NutchClient Feld in NutchSettings eingabaut

--- HSA.InfoSys/HSA.InfoSys.Entities/NutchControllerClientSettings.cs
+++ HSA.InfoSys/HSA.InfoSys.Entities/NutchControllerClientSettings.cs
@@ -25,6 +25,15 @@ namespace HSA.InfoSys.Common.Entities
         public virtual string SolrServer { get; set; }
 
         /// <summary>
+        /// Gets or sets the nutch clients.
+        /// </summary>
+        /// <value>
+        /// The nutch clients.
+        /// </value>
+        [DataMember]
+        public virtual string NutchClients { get; set; }
+
+        /// <summary>
         /// Gets or sets the name of the seed file.
         /// </summary>
         /// <value>
@@ -49,7 +58,7 @@ namespace HSA.InfoSys.Common.Entities
         /// The prefix path.
         /// </value>
         [DataMember]
-        public virtual string PrefixPath { get; set; } // .nutch/
+        public virtual string PrefixPath { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the prefix file.
